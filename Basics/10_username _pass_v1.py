2304b20355c1be1e2a64861f1172aace0830974f
OnePy26 <subhadip8602@outlook.com>
2026-06-24 21:12:48 +0530

    added some modification and cleanup

--- Basics/10_username _pass_v1.py
+++ Basics/10_username _pass_v1.py
@@ -4,19 +4,22 @@ username = "petrol"
 password = "P3254L"
 
 count = 0
-
+#Putting input for name before the while loop ensures that Name input is asked only once.
+name = input("Enter your name: ")
 while True:
-    name = input("Enter your name: ")
+    
     user_input = input("Enter username: ")
     pass_input = input("Enter password: ")
 
     if user_input == username and pass_input ==password:
         print(f"Login Successfull: Welcome {name}")
+# Break is placed here so the program terminates upon successfull username and pass ignoring the rest of the loop.
+        break
     else:
         count = count + 1
 # number of attempts can be changed by changing 3 to any desired number number
         if count ==3:
-            print("Maximum attlempt reached")
+            print("Maximum attempt reached")
             break
         if user_input != username:
             print("Wrong username")
