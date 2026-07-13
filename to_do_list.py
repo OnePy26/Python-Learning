90fac1582e082ceda7463bfbd2fb44da44bfe072
OnePy26 <subhadip8602@outlook.com>
2026-07-13 22:46:01 +0530

    To Do list - start

--- to_do_list.py
+++ to_do_list.py
@@ -1,23 +1,37 @@
 # Simple beginner friendly To DO List code - Using Lists.
 # Variables 
 name  = "alan"
-username = "alan24"
-password = "alan42"
+username1 = "alan24"
+password1 = "alan42"
 
-list
+# list
 task_list = [] #original list
 complete_task =[] # completed task's will be moved from last_list to this list for the show_complete function.
 
 def add_task ():
-    pass
+    while True:
+            task = input("Add task (N to main menu): ")
+            if task.lower() == "n":
+                break
+            elif len(task) < 5:
+                print("Task should be of at least 5 Characters|Try Again")
+            task_list.append(task)
+            
+
 
 def remove_task ():
-    pass
+    while True:
+            task = input("Add task (N to main menu): ")
+            if task.lower() == "n":
+                break
+            elif len(task) < 5:
+                print("Task should be of at least 5 Characters|Try Again")
+            task_list.append(task)
 
 def mark_complete ():
     pass
 
-def show_complete ():
+def show_completed ():
     pass
 
 def show_pending ():
@@ -28,16 +42,16 @@ def password ():
     while True:
         user1 = input("Username :")
         pass1 = input("Password :")
-        if user1 == username and pass1 == password:
+        if user1 == username1 and pass1 == password1:
             print("Welcome", name )
             return True
-            if user1 != username or pass1 != password:
-                count += 1
-                if count == 3:
-                    print("MAximum attempt reached | User Locked")
-                    return False
-                else:
-                    print ("Wrong Username/password: Try Again")
+        else:
+            count += 1
+            if count == 3:
+                print("Maximum attempt reached | User Locked")
+                return False
+            else:
+                print ("Wrong Username/password: Try Again")
 
 #Menu
 
@@ -47,11 +61,30 @@ if result == True:
 
     print (f"==================Task Manager================ \n"
     f"1. Add task                   2. Remove Task \n"
-    f"3. Mark Complete              4. Show Complete \n"
+    f"3. Mark Complete              4. Show Completed \n"
     f"                   5. Exit \n"
     f"============================================== \n")
 
 #Choice
+while True:
+    try:
+        choice = input("enter choice: ")
+        choice = int(choice)
+        if choice == 1:
+            add_task()
+        elif choice ==2:
+            remove_task()
+        elif choice == 3:
+            mark_complete()
+        elif choice == 4:
+            show_completed()
+        else:
+            choice == 5
+            break
+
+
+
+
 
 
 # loop for Menu
