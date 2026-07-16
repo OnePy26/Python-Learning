83c5190b5187e627dd770471862842ef8f39938f
OnePy26 <subhadip8602@outlook.com>
2026-07-16 23:50:06 +0530

    to do list update

--- to_do_list.py
+++ to_do_list.py
@@ -19,55 +19,64 @@ def add_task ():
             if tasks.lower() == "e":
                 break
             elif len(tasks) <= 5:
-                print("Task should be of at least 5 Characters|Try Again")
+                print(f"Task should be of at least 5 Characters|Try Again")
             tasks_new = tasks.split(",")
             for i in tasks_new:
                 task_list.append(i.strip().title())
 
 
 def remove_task ():
-    pass            
-
-def show_tasks ():
     global task_list
-    for i in task_list:
-        print(task_list.index(i, start = 1), i)   
+    while True:
+            number = input("Add task number ('E' to main menu): ")
+            if number.lower() == "e":
+                break
+            number= int(number) - 1
+            task_list.pop(number)
+                
 
 def replace_task ():
     global task_list
     while True:
-            number = input("Add task number (E to main menu): ")
+            number = input(f"Add task number ('E' to main menu): ")
             if number.lower() == "e":
                 break
-            number= int(number)
-            new_task = input("Add New task (E to main menu): ")
-            if new_task.lower() == "e":
-                break
-            task_list[number] = new_task            
-
-
+            number= int(number) - 1
+            new_task = input(f"Add New task ('E' to main menu): ")
+            if len(new_task) <= 5:
+                print(f"Task should be of at least 5 Characters|Try Again")
+            task_list[number] = new_task
+                
 
 def mark_complete ():
     while True:
         try:
-            number = input("Add task number (E to main menu): ")
+            number = input(f"Add task number ('E' to main menu): ")
             if number.lower() == "e":
                 break
-            number = int.number
-            "[\u2713]" + task_list.index[number]
+            number = int(number) - 1
+            task_list[number] = "[\u2713]" + task_list[number]
         except:
-            print("Invalid Input| Try Again")
+            print(f"Invalid Input| Try Again")
+
 
 def show_completed ():
     for i in task_list:
         if i.startswith("[\u2713]"):
-            print(task_list.index(i), i)
+            print(task_list.index(i) + 1, i)
 
 
 def show_pending ():
     for i in task_list:
         if not i.startswith("[\u2713]"):
-            print(task_list.index(i), i)
+            print(task_list.index(i) + 1, i.title())
+
+
+def show_all_tasks ():
+    if len(task_list) == 0:
+        print(f"No Tasks Found | Add Tasks First")
+    for i in task_list:
+        print(task_list.index(i) + 1, i.title())
 
 
 def password ():
@@ -76,15 +85,15 @@ def password ():
         user1 = input("Username :")
         pass1 = input("Password :")
         if user1 == username1 and pass1 == password1:
-            print("Welcome", name )
+            print(f"Welcome {name.title()}")
             return True
         else:
             count += 1
             if count == 3:
-                print("Maximum attempt reached | User Locked")
+                print(f"Maximum attempt reached | User Locked")
                 return False
             else:
-                print ("Wrong Username/password: Try Again")
+                print (f"Wrong Username/Password: Try Again")
 
 #Menu
 
@@ -95,12 +104,13 @@ if result:
     print (f"==================Task Manager================ \n"
     f"1. Add task                   2. Remove Task \n"
     f"3. Replace Task               4. Mark Complete \n"
-    f"5. Show Completed               6. Show Pending \n"
-    f"                     7.Exit                       \n"
+    f"5. Show Completed             6. Show Pending \n"
+    f"7. Show All Tasks             8. Exit\n"
     f"============================================== \n")
 
 #Choice
-while True:
+
+while True: # loop for Menu
     try:
         choice = input("enter choice: ")
         choice = int(choice)
@@ -109,22 +119,17 @@ while True:
         elif choice ==2:
             remove_task()
         elif choice == 3:
-            show_tasks()
+            replace_task()
         elif choice == 4:
             mark_complete()
         elif choice == 5:
             show_completed()
         elif choice == 6:
-            replace_task()
-        else:
-            choice == 7
+            show_pending()
+        elif choice == 7:
+            show_all_tasks()
+        elif choice == 8:
             break
     except:
         print("Invalid Choice | Try again")
 
-
-
-
-
-
-# loop for Menu
